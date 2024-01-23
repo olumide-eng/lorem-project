4610a24985b4095c42fdf0b7b0ceaf8c820ba605
olumide <olumide@olumides-MacBook-Pro.local>
2024-01-23 10:03:48 +0000

    my first lorem project

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import data from "./data";
+
+const App = () => {
+  const [count, setCount] = useState(4);
+  const [text, setText] = useState([]);
+
+  return (
+    <section className="section-center">
+      <h4>tired of boring lorem ipsm</h4>
+      <form className="lorem-form">
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min="1"
+          step="1"
+          max="8"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn" type="submit">
+          generate
+        </button>
+      </form>
+    </section>
+  );
+};
+export default App;
